cbb753f923d2ba52a0beb10a42621dfae6992ecd
Mikhail Korobov <kmike84@gmail.com>
2012-03-20 16:49:42 +0600

    тесты через tox

--- yandex_maps_tests/settings.py
+++ yandex_maps_tests/settings.py
@@ -6,8 +6,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, '..')))
 DEBUG = True
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = 'db.sqlite'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite'
+    }
+}
 ROOT_URLCONF = 'urls'
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),)
+
+SECRET_KEY = '123'
 
 # мой ключ для домена example.com
 YANDEX_MAPS_API_KEY = "AOmQeUwBAAAAxp1AOQIAlTD0uSCCp1ukf-GmyqC5tbvCGAgAAAAAAAAAAAAeazHdQX6EPpcOJnuUPR-QlXyJPQ=="
@@ -16,5 +25,5 @@ INSTALLED_APPS=(
     'yandex_maps',
     'test_app',
 #    'devserver',
-#    'south'
+    'south',
 )
